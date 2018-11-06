aa8b111a8249adf6b28530a4fd03462907d6c11e
Mika Leppänen <mikap.leppanen.ext@nokia.com>
2018-11-06 13:53:53 +0200

    Included jQuery

--- action.js
+++ action.js
@@ -4,10 +4,12 @@ var myVar2 = setInterval(myTimer1, 90);
 var myVar3 = setInterval(myTimer2, 2100);
 var myVar4 = setInterval(myTimer2, 2200);
 
+
 function myTimer1() {
-	document.querySelector("h1").textContent = "Hello, world!";
+	$("h1")[0].textContent = "Hello, world!";
 } 
 
 function myTimer2() {
-	document.querySelector("h1").textContent = "Hello, worldz!";
+	$("h1")[0].textContent = "Hello, worldz!";
 } 
+
